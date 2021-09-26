4cfdf4a5967e61181e37b1363499fb556c56bdde
podolskijspace <goncharovnikita713@gmail.com>
2021-09-26 15:39:30 +0300

    Сделал копирование элементов, но с ошибкой

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Layer, Stage} from 'react-konva';
 import Rectangle from '../rectangle/Rectangle';
 
@@ -46,6 +46,38 @@ function App() {
   const [rects, setRectangles] = React.useState(initialRectangles);
   const [selectedId, changeSelected] = useState(["1", "3"]);
 
+  useEffect(() => {
+    const onKeypress = e => {
+      const newArr = [];
+
+      const pushElem = (attr) => {
+        newArr[newArr.length] = {...attr}
+        
+        newArr[newArr.length - 1].y += 100;
+        newArr[newArr.length - 1].id = rects.length;
+        
+      }
+
+      if (e.key === 'Tab' && e.ctrlKey) {
+        rects.forEach((rect, id) => {
+          if (!!~selectedId.indexOf(id + '')) {
+            pushElem(rect);
+          }
+        })
+
+        const rectangles = [...rects, ...newArr];
+
+        setRectangles(rectangles);
+      }
+    }
+  
+    document.addEventListener('keypress', onKeypress);
+  
+    return () => {
+      document.removeEventListener('keypress', onKeypress);
+    };
+  }, [selectedId]);
+
   const checkMissClick = (e) => { //Проверка, если клик по пустому месту
     if (e.target === e.target.getStage()) {
       changeSelected([]);
@@ -53,47 +85,49 @@ function App() {
   }
 
   return (
-    <Stage 
-    width={500} 
-    height={200}
-    onMouseDown={checkMissClick}
-    onTouchStart={checkMissClick}
-    >
-      <Layer >
-        {
-          rects.map((rect, i) => {
-            return <Rectangle 
-              shapeProps = {rect}
-              id={rect.id}
-              key={rect.id}
-              onChange={(newAttrs) => {
-                const rectangles = rects.slice();
-                rectangles[i] = newAttrs;
-                setRectangles(rectangles);
-              }}
-              onSelect={(e) => {
-                if (e.evt.shiftKey) { //Проверка, что зажата кнопка шифт
-                  const key = selectedId.indexOf(rect.id);
-                  if(!~key) { //Проверка, что этот индекс не повторяется
-                    changeSelected([...selectedId,...rect.id])
+    < div onKeyUp={()=> {console.log('sd')}}>
+      <Stage 
+      width={500} 
+      height={1000}
+      onMouseDown={checkMissClick}
+      onTouchStart={checkMissClick}
+      >
+        <Layer >
+          {
+            rects.map((rect, i) => {
+              return <Rectangle 
+                shapeProps = {rect}
+                id={rect.id}
+                key={rect.id}
+                onChange={(newAttrs) => {
+                  const rectangles = rects.slice();
+                  rectangles[i] = newAttrs;
+                  setRectangles(rectangles);
+                }}
+                onSelect={(e) => {
+                  if (e.evt.shiftKey) { //Проверка, что зажата кнопка шифт
+                    const key = selectedId.indexOf(rect.id);
+                    if(!~key) { //Проверка, что этот индекс не повторяется
+                      changeSelected([...selectedId,...rect.id])
+                    } else {
+                      changeSelected([...selectedId.slice(0,key), ...selectedId.slice(key + 1)]);//Если повторяется, то убираем
+                    }
                   } else {
-                    changeSelected([...selectedId.slice(0,key), ...selectedId.slice(key + 1)]);//Если повторяется, то убираем
+                    if (selectedId.length === 1 && selectedId[0] === rect.id) {
+                      changeSelected([])
+                    } else {
+                      changeSelected([...rect.id])
+                    }
                   }
-                } else {
-                  if (selectedId.length === 1 && selectedId[0] === rect.id) {
-                    changeSelected([])
-                  } else {
-                    changeSelected([...rect.id])
-                  }
-                }
-              }}
-              isSelect={selectedId}
-              
-            />
-          })
-        }
-      </Layer>
-    </Stage>
+                }}
+                isSelect={selectedId}
+
+              />
+            })
+          }
+        </Layer>
+      </Stage>
+    </div>
   );
 }
 
